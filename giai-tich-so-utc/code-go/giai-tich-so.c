55b91ebd27f12463beedbbb2ed63a78aa782aec9
chunglop0781 <chunglop0781@gmail.com>
2026-06-16 15:45:26 +0700

    Update giai-tich-so.c

--- giai-tich-so-utc/code-go/giai-tich-so.c
+++ giai-tich-so-utc/code-go/giai-tich-so.c
@@ -278,30 +278,30 @@ int main(){
 	        printf("Nghiem c tim duoc la: %lf\n", c);
     } else if(luaChon == 7){
         	double a, b, ss;
-	double x0, Fu, x1;
-	int dem = 0;
-	printf("Nhap khoang cach [a, b]:\n");
-	printf("a = "); scanf("%lf", &a);
-	printf("b = "); scanf("%lf", &b);
-	printf("Nhap sai so (ss): "); scanf("%lf", &ss);
-	if (F(a) > 0){
-		x0 = b;
-		Fu = a;
-	} else {
-        x0 = a;
-        Fu = b;
-    }
-    do {
-    	x1 = x0 - (F(x0) / (F(Fu) - F(x0))) * (Fu - x0);
-    	if(fabs(x1 - x0) < ss){
-    		break;
-		} else {
-			x0 = x1;
-		}
-		dem++;
-	} while (dem < 100000);
-	printf("\nGiao diem cua f(x) va g(x) la: x1 = %.7lf\n", x1);
-	printf("So buoc lap = %d\n", dem);
+	        double x0, Fu, x1;
+	        int dem = 0;
+	        printf("Nhap khoang cach [a, b]:\n");
+	        printf("a = "); scanf("%lf", &a);
+	        printf("b = "); scanf("%lf", &b);
+	        printf("Nhap sai so (ss): "); scanf("%lf", &ss);
+	        if (F(a) > 0){
+	        	x0 = b;
+	        	Fu = a;
+        	} else {
+                x0 = a;
+                Fu = b;
+            }
+            do {
+            	x1 = x0 - (F(x0) / (F(Fu) - F(x0))) * (Fu - x0);
+            	if(fabs(x1 - x0) < ss){
+            		break;
+	        	} else {
+	        		x0 = x1;
+	        	}
+	        	dem++;
+	        } while (dem < 100000);
+	        printf("\nGiao diem cua f(x) va g(x) la: x1 = %.7lf\n", x1);
+	        printf("So buoc lap = %d\n", dem);
     }
     return 0;
 }
